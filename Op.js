dc83c5c8b2436ebe3950e0b0b0aa8b3e189bf97d
Giselle <gisalina0618@gmail.com>
2023-01-21 20:11:44 -0400

    Op

---
+++ Op.js
@@ -0,0 +1,73 @@
+let mestexto = "10";
+let diahoy = 17;
+let diaayer = 16;
+let meshoy = 10;
+let mesayer = 10;
+let comparacion;
+
+
+
+comparacion = diahoy>diaayer;
+
+document.write("diaHoy ",diahoy, " es mayor que diaAyer ", diaayer,": ", comparacion)
+
+document.write("<br>")
+
+comparacion = diahoy < diaayer;
+
+document.write("diaHoy ", diahoy, " es menor que diaAyer ", diaayer, ": ", comparacion)
+
+document.write("<br>")
+
+comparacion = diahoy >= diaayer;
+
+document.write("diaHoy ", diahoy, " es mayor o igual que diaAyer ", diaayer, ": ", comparacion)
+
+document.write("<br>")
+
+comparacion = meshoy >= mesayer;
+
+document.write("mesHoy ", meshoy," es mayor o igual que mesAyer ", mesayer, ": ", comparacion)
+
+document.write("<br>")
+
+comparacion = diahoy <= diaayer;
+
+document.write("diaHoy ", diahoy, " es menor o igual que diaAyer ", diaayer,": ", comparacion)
+
+document.write("<br>")
+
+comparacion = meshoy <= mesayer;
+
+document.write("mesHoy ", meshoy, " es menor o igual que mesAyer ", mesayer, ": ", comparacion)
+
+document.write("<br>")
+
+comparacion = mestexto == mesayer;
+
+document.write("mesTexto ", mestexto, " es igual que mesAyer ", mesayer, ": ", comparacion)
+
+document.write("<br>")
+
+comparacion = diahoy == diaayer;
+
+document.write("diaHoy ", diahoy, " es igual que diaAyer ", diaayer, ": ", comparacion)
+
+document.write("<br>")
+
+comparacion = meshoy == mesayer;
+
+document.write("mesHoy ", meshoy, " es igual que mesAyer ", mesayer, ": ", comparacion)
+
+document.write("<br>")
+
+comparacion = meshoy === mesayer;
+
+document.write("mesHoy ", meshoy, " es idéntico que mesAyer ", mesayer, ": ", comparacion)
+
+document.write("<br>")
+
+comparacion = mestexto === mesayer;
+
+document.write("mesTexto ", mestexto, " es idéntico que mesAyer ", mesayer, ": ", comparacion)
+
